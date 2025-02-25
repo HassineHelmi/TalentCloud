0d56456fa1ab53f1cac387724fc410d0ad00af8d
Sondos-Snd <awachri.sondos@gmail.com>
2025-02-25 18:24:38 +0100

    basic repo

---
+++ recruitment-platform/profile-service/src/test/java/com/profile_service/profile_service/TestProfileServiceApplication.java
@@ -0,0 +1,11 @@
+package com.profile_service.profile_service;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestProfileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(ProfileServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
