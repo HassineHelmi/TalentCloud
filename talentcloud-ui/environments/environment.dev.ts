b50eb27c7c38a224feacf88862973b5c3ae76d9e
Sondos-Snd <awachri.sondos@gmail.com>
2025-02-27 14:53:01 +0100

    basic repo

---
+++ talentcloud-ui/environments/environment.dev.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://dev.api.recruitment.com',
+    auth: {
+      clientId: 'recruitment-client',
+      issuer: 'https://auth.dev.recruitment.com'
+    }
+  };
+  
