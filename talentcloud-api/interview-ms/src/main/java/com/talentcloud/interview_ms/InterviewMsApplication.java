17065c5124a294866c8dbd8e73e28a07fc7b6b5b
Sondos-Snd <awachri.sondos@gmail.com>
2025-04-04 19:01:17 +0100

    add interview-ms and kafka call

---
+++ talentcloud-api/interview-ms/src/main/java/com/talentcloud/interview_ms/InterviewMsApplication.java
@@ -0,0 +1,13 @@
+package com.talentcloud.interview_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewMsApplication.class, args);
+	}
+
+}
